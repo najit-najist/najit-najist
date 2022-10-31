39d1e67b79c28746368bd3b388b57f99a2dee834
Ondřej Langr <hi@ondrejlangr.cz>
2022-10-31 22:20:46 +0100

    Create astro only static and use serve to serve those static files.

--- apps/intro-web/astro.config.mjs
+++ apps/intro-web/astro.config.mjs
@@ -2,15 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import image from '@astrojs/image';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   site: 'http://najitnajit.cz',
-  adapter: node({
-    mode: 'standalone',
-  }),
   server: {
     host: '0.0.0.0',
   },
