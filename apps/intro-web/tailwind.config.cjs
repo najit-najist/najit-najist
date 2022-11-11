c44e24210d0d5c0d3b1880c7dad34c2326e9e225
Ondřej Langr <hi@ondrejlangr.cz>
2022-11-11 13:11:25 +0100

    feature: Add base for checkboxes.

--- apps/intro-web/tailwind.config.cjs
+++ apps/intro-web/tailwind.config.cjs
@@ -14,7 +14,7 @@ module.exports = {
   },
   plugins: [
     {
-      handler({ addUtilities }) {
+      handler({ addUtilities, addBase }) {
         addUtilities({
           '.text-shadow-sm': {
             textShadow: '#ffffff 0px 0px 6px',
@@ -22,6 +22,12 @@ module.exports = {
           '.text-shadow': {
             textShadow:
               '#c8c8c8 1px 1px 0px, #b4b4b4 0px 2px 0px, #a0a0a0 0px 3px 0px, rgb(140 140 140 / 50%) 0px 4px 0px, #787878 0px 0px 0px, rgb(0 0 0 / 50%) 0px 5px 10px',
+          },
+        });
+
+        addBase({
+          'input[type=checkbox]:not(:checked) ~ .checkmark svg': {
+            display: 'none!important',
           },
         });
       },
