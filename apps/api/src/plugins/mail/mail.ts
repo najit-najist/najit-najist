d61ae12a95cef64b874bab5d0add7052b0a41463
Ondřej Langr <hi@ondrejlangr.cz>
2022-11-11 11:27:19 +0100

    feature: Use nodemailer to create correct transporter.

--- apps/api/src/plugins/mail/mail.ts
+++ apps/api/src/plugins/mail/mail.ts
@@ -2,6 +2,8 @@ import { config } from '@config';
 import fp from 'fastify-plugin';
 import path from 'path';
 import Email from 'email-templates';
+import nodemailer from 'nodemailer';
+import { APP_ROOT } from '@constants';
 
 declare module 'fastify' {
   interface FastifyInstance {
@@ -12,6 +14,16 @@ declare module 'fastify' {
 export const mailPlugin = fp(async (server) => {
   server.log.info(`[Mail plugin]: Initialize (${config.env.isDev})`);
 
+  let transporter = nodemailer.createTransport({
+    host: config.mail.host,
+    port: config.mail.port,
+    secure: config.mail.port === 465,
+    auth: {
+      user: config.mail.user,
+      pass: config.mail.password,
+    },
+  });
+
   const email = new Email({
     send: !config.env.isDev,
     preview: config.env.isDev,
@@ -19,22 +31,12 @@ export const mailPlugin = fp(async (server) => {
       from: '"Najít&Najist pošťák" <info@najitnajist.cz>',
     },
     views: {
-      root: path.join(__dirname, 'templates'),
+      root: path.join(APP_ROOT, 'email-templates'),
       options: {
         extension: 'ejs',
       },
     },
-    transport: {
-      jsonTransport: true,
-      host: config.mail.host,
-      port: config.mail.port,
-      secure: true,
-      auth: {
-        type: 'login',
-        user: config.mail.user,
-        pass: config.mail.password,
-      },
-    },
+    transport: transporter,
   });
 
   await server.decorate('mail', email);
