2c58d7e5b3880ac0aac5adb3fdfaa4d152dce8c4
Ondřej Langr <hi@ondrejlangr.cz>
2022-12-05 22:35:30 +0100

    Fix build

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { FC, PropsWithChildren } from 'react';
 import { Header, Footer } from '@components/layout';
 
 import '../globals.css';
+import { LayoutComponent } from '../../.next/types/app/layout';
 
-const RootLayout: FC<PropsWithChildren> = ({ children }) => {
+const RootLayout: LayoutComponent = ({ children }) => {
   return (
     <html lang="cs">
       <body className="bg-gradient-to-b from-[#fbf9eb] to-white data-scroll-container">
