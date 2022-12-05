671d846841fa6c59dc3752ebb147e08c43576cac
Ondřej Langr <hi@ondrejlangr.cz>
2022-12-05 21:51:24 +0100

    Update content of web

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 const MainPage: FC = () => {
-  return <>sdfdsf</>;
+  return <></>;
 };
 
 export default MainPage;
---
+++ apps/web/src/app/recepty/page.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const RecipesPage: FC = () => {
+  return <>recipes</>;
+};
+
+export default RecipesPage;
