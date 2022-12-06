cd354338640de0087d6190a344e451048361c51d
Ondřej Langr <hi@ondrejlangr.cz>
2022-12-06 07:16:23 +0100

    Update Trpc context initialization

--- apps/web/src/contexts/ContextProviders.tsx
+++ apps/web/src/contexts/ContextProviders.tsx
@@ -2,14 +2,19 @@
 
 import { TRPC_API_URL } from '@constants';
 import { FC, PropsWithChildren, useState } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { trpc } from '../trpc';
+import { httpBatchLink } from '@trpc/client';
 
 export const ContextProviders: FC<PropsWithChildren> = ({ children }) => {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
-      url: TRPC_API_URL.toString(),
+      links: [
+        httpBatchLink({
+          url: TRPC_API_URL.toString(),
+        }),
+      ],
     })
   );
 
