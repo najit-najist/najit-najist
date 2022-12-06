73a977fe9222e993b2b17dbaa6d6a7783967e5f2
Ondřej Langr <hi@ondrejlangr.cz>
2022-12-06 21:35:48 +0100

    Add hero-banner component

--- apps/web/src/app/components/HeroBanner.tsx
+++ apps/web/src/app/components/HeroBanner.tsx
@@ -1,12 +1,23 @@
+import Image from 'next/image';
 import { FC } from 'react';
+
+import heroBannerImage from '../../../public/images/pages/main-page/hero-banner-picture.png';
 
 export const HeroBanner: FC = () => {
   return (
-    <>
-      <h2>
-        U nás můžete NAJÍT řešení jak jíst bez lepku a laktózy a konečně se
-        NAJÍST téměř jako dřív...
-      </h2>
-    </>
+    <div className="relative overflow-hidden -mt-40">
+      <Image
+        src={heroBannerImage}
+        alt="úvodní obrázek najít najíst"
+        className="absolute left-0 top-0 w-full h-full object-cover object-center"
+      />
+      <div className="h-40" />
+      <div className="container relative pb-52 pt-24">
+        <h2 className="font-suez text-6xl max-w-[52rem] leading-relaxed text-deep-green-300">
+          U nás můžete NAJÍT řešení jak jíst bez lepku a laktózy a konečně se
+          NAJÍST téměř jako dřív...
+        </h2>
+      </div>
+    </div>
   );
 };
