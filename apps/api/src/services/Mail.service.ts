3b995d958fe6ce2b8472ee6cb6b45a255134e58a
Ondřej Langr <hi@ondrejlangr.cz>
2022-12-06 06:45:47 +0100

    DIfferent private properties approach

--- apps/api/src/services/Mail.service.ts
+++ apps/api/src/services/Mail.service.ts
@@ -1,14 +1,13 @@
 import { FastifyBaseLogger, FastifyInstance } from 'fastify';
 import Email from 'email-templates';
-import { config } from '@config';
 
 export class MailService {
-  private logger: FastifyBaseLogger;
-  private mailer: Email;
+  #logger: FastifyBaseLogger;
+  #mailer: Email;
 
   constructor(server: FastifyInstance) {
-    this.logger = server.log;
-    this.mailer = server.mail;
+    this.#logger = server.log;
+    this.#mailer = server.mail;
   }
 
   async send({
@@ -24,7 +23,7 @@ export class MailService {
   }) {
     let result;
     try {
-      result = await this.mailer.send({
+      result = await this.#mailer.send({
         template,
         message: {
           to,
@@ -32,15 +31,15 @@ export class MailService {
         },
         locals: payload,
       });
-      this.logger.info(
+      this.#logger.info(
         `MailService: Mail has been sent to email: ${to} -- ${JSON.stringify({
           rejected: result?.rejected,
           response: result?.response,
         })}`
       );
     } catch (e) {
-      this.logger.error('Mail sending has error', e);
-      this.logger.error(e);
+      this.#logger.error('Mail sending has error', e);
+      this.#logger.error(e);
 
       throw Error('Posílání emailu nebylo úspěšné.');
     }
