818d9c0f10b5d9ebd29da25bdbb5d9258c906c06
Ondřej Langr <hi@ondrejlangr.cz>
2022-12-10 23:36:17 +0100

    Feature/footer (#18)
    * Add redirect to github on github route
    
    * Add logo image
    
    * Add logo component
    
    * Add logo to header
    
    * Update header
    
    * Update tailwind config
    
    * Add more padding when on big screen
    
    * Add newsletter
    
    * Update quick links
    
    * Redirect to githib is not permanent

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/najit-najist/najit-najist',
+        permanent: false,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
