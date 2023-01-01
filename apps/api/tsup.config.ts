164b197ca6aeaa6018fde8a87eccbb48e9079a73
Ondřej Langr <hi@ondrejlangr.cz>
2023-01-01 10:57:20 +0100

    Update tsup config for api

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/server.ts'],
+  format: ['cjs', 'esm'],
+  tsconfig: './tsconfig.json',
+  target: 'es5',
   splitting: false,
   sourcemap: true,
-  target: 'es5',
-  format: ['cjs', 'esm'],
   clean: true,
   dts: true,
-  tsconfig: './tsconfig.json',
   treeshake: true,
 });
