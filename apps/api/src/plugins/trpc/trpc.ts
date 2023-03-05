668b0a23351b7c9c51dcecde01c281fa7c77b578
Ondřej Langr <hi@ondrejlangr.cz>
2023-03-05 10:51:08 +0100

    update: Update import of trpc fastify adapter since its fixed.

--- apps/api/src/plugins/trpc/trpc.ts
+++ apps/api/src/plugins/trpc/trpc.ts
@@ -1,5 +1,5 @@
 import fp from 'fastify-plugin';
-import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify/dist/trpc-server-adapters-fastify.cjs.js';
+import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify';
 import { createContext } from './context';
 import { appRouter } from './router';
 
