ec2041f89274a75227e1a999037766f5775619ea
Ondřej Langr <hi@ondrejlangr.cz>
2023-03-06 23:23:28 +0100

    update: Update life route.

--- apps/api/src/routes/info/life.ts
+++ apps/api/src/routes/info/life.ts
@@ -3,15 +3,13 @@ import { FastifyPluginAsync } from 'fastify';
 
 export const lifeRoute: FastifyPluginAsync = async (server) => {
   server.get('/', async (req, res) => {
-    let pocketbaseConnected = await fetch(
-      new URL('/api/health', config.pb.origin)
-    )
+    let pocketbase = await fetch(new URL('/api/health', config.pb.origin))
       .catch(() => false)
-      .then(() => true);
+      .then((res) => res);
 
     res.send({
       alive: true,
-      pocketbaseConnected,
+      pocketbase,
     });
   });
 };
