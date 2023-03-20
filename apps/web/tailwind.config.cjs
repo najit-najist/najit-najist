86535175b535e840e50d6189f7953ea602e22fa4
Ondřej Langr <hi@ondrejlangr.cz>
2023-03-20 19:09:29 +0100

    internal: Update tailwind content.

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -6,6 +6,6 @@ module.exports = {
   ...getTheme(),
   content: [
     './src/**/*.{js,jsx,md,mdx,ts,tsx}',
-    path.join(path.dirname(require.resolve('@najit-najist/ui')), '*.js'),
+    path.join(path.dirname(require.resolve('@najit-najist/ui')), '**', '*.js'),
   ],
 };
--- packages/najit-najist-ui/tailwind.config.cjs
+++ packages/najit-najist-ui/tailwind.config.cjs
@@ -1,11 +1,7 @@
-const path = require('node:path');
 const { getTheme } = require('@najit-najist/tailwind-plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...getTheme(),
-  content: [
-    './src/**/*.{js,jsx,md,mdx,ts,tsx}',
-    path.join(path.dirname(require.resolve('@najit-najist/ui')), '*.js'),
-  ],
+  content: ['./src/**/*.{js,jsx,md,mdx,ts,tsx}'],
 };
