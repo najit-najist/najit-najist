3358c45d457e211f9126c1d5e18844939a551f3d
Ondřej Langr <hi@ondrejlangr.cz>
2023-03-25 22:51:58 +0100

    feature: Add timeout to fetch on server health route.

--- apps/web/src/pages/api/info/health.ts
+++ apps/web/src/pages/api/info/health.ts
@@ -1,9 +1,39 @@
-import { config } from '@najit-najist/api/server';
+import { config, logger } from '@najit-najist/api/server';
 import { NextApiHandler } from 'next';
 
+const TIMEOUT = 3 * 60 * 1000;
+const fetchWithTimeout = async (
+  input: RequestInfo | URL,
+  init?: RequestInit | undefined
+) => {
+  const abortController = new AbortController();
+
+  // Catch from outside
+  init?.signal?.addEventListener('abort', (reason) => {
+    abortController.abort(reason);
+  });
+
+  const timeout = setTimeout(() => {
+    abortController.abort();
+  }, TIMEOUT);
+
+  return fetch(input, {
+    ...init,
+    signal: abortController.signal,
+  }).finally(() => {
+    clearTimeout(timeout);
+  });
+};
+
 const handler: NextApiHandler = async () => {
-  let pocketbase = await fetch(new URL('/api/health', config.pb.origin))
-    .catch(() => false)
+  let pocketbase = await fetchWithTimeout(
+    new URL('/api/health', config.pb.origin)
+  )
+    .catch((error) => {
+      logger.error(error, 'Failed to connect to pocketbase');
+
+      return false;
+    })
     .then((res) => res);
 
   return {
