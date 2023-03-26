4293481c6843fb1f3a4120913636819aebf8a1ee
Ondřej Langr <hi@ondrejlangr.cz>
2023-03-26 12:26:27 +0200

    fix: Fix getClient to pass cookies.

--- apps/web/src/vanilla-trpc.ts
+++ apps/web/src/vanilla-trpc.ts
@@ -1,16 +1,26 @@
 import { AppRouter, serverPort } from '@najit-najist/api';
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import { headers as getHeaders } from 'next/headers';
 import SuperJSON from 'superjson';
 
 export const getClient = (
   params?: Pick<Parameters<typeof httpBatchLink>[0], 'headers' | 'fetch'>
-) =>
-  createTRPCProxyClient<AppRouter>({
+) => {
+  const headersStore = getHeaders();
+
+  return createTRPCProxyClient<AppRouter>({
     transformer: SuperJSON,
     links: [
       httpBatchLink({
         url: new URL('/api/trpc', `http://127.0.0.1:${serverPort}`).toString(),
+        headers: {
+          cookie:
+            headersStore.get('cookie') ??
+            headersStore.get('Cookie') ??
+            undefined,
+        },
         ...params,
       }),
     ],
   });
+};
