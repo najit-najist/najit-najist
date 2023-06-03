9472afe75d132cf8dfca1234b2f85d4405ebcb13
Ondřej Langr <hi@ondrejlangr.cz>
2023-06-03 16:05:30 +0200

    Remove port from login redirect

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -48,6 +48,7 @@ export async function middleware(request: NextRequest) {
 
       const url = request.nextUrl.clone();
       url.hostname = 'dev.najitnajist.cz';
+      url.port = '';
 
       // If key is not code then do nothing
       if (key !== 'code') {
