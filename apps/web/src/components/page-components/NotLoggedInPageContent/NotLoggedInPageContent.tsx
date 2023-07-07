3b4bc9e3c21804449069225c3af9185bbb78510f
Ondřej Langr <hi@ondrejlangr.cz>
2023-07-07 08:54:18 +0200

    Layout styling

--- apps/web/src/components/page-components/NotLoggedInPageContent/NotLoggedInPageContent.tsx
+++ apps/web/src/components/page-components/NotLoggedInPageContent/NotLoggedInPageContent.tsx
@@ -3,7 +3,7 @@ import { Links } from './Links';
 
 export const NotLoggedInPageContent: FC = () => {
   return (
-    <div className="min-h-[40vh] flex items-center justify-center">
+    <div className="min-h-[40vh] flex items-center justify-center py-20">
       <div className="">
         <h2 className="font-semibold text-deep-green-300 text-4xl text-center mb-10">
           Tato stránka je pouze pro přihlášené uživatele...
