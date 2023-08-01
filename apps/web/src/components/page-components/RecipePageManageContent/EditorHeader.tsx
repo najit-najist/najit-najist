386922927b6f110bb6b98e388e0d10b32ef5ee28
Ondřej Langr <hi@ondrejlangr.cz>
2023-08-01 11:59:38 +0200

    Add loading when submitting

--- apps/web/src/components/page-components/RecipePageManageContent/EditorHeader.tsx
+++ apps/web/src/components/page-components/RecipePageManageContent/EditorHeader.tsx
@@ -4,7 +4,24 @@ import { Button, buttonStyles } from '@najit-najist/ui';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import { FC } from 'react';
+import { useFormContext } from 'react-hook-form';
 import { ViewType } from './_types';
+
+const EditorContent: FC = () => {
+  const { formState } = useFormContext();
+
+  return (
+    <>
+      <Button
+        className="ml-auto"
+        type="submit"
+        isLoading={formState.isSubmitting}
+      >
+        Uložit
+      </Button>
+    </>
+  );
+};
 
 export const EditorHeader: FC<{ viewType: ViewType }> = ({ viewType }) => {
   const pathname = usePathname();
@@ -30,11 +47,7 @@ export const EditorHeader: FC<{ viewType: ViewType }> = ({ viewType }) => {
             {isEditorEnabled ? 'Ukončit úpravu' : 'Upravit'}
           </Link>
         ) : null}
-        {isEditorEnabled || viewType === 'create' ? (
-          <Button className="ml-auto" type="submit">
-            Uložit
-          </Button>
-        ) : null}
+        {isEditorEnabled || viewType === 'create' ? <EditorContent /> : null}
       </div>
     </div>
   );
