e03933b7e09fbd71bfcc5071184fc92cd3bcf716
Ondřej Langr <hi@ondrejlangr.cz>
2023-08-11 12:58:28 +0200

    Update contact page

--- apps/web/src/app/(with-visible-layout)/kontakt/page.tsx
+++ apps/web/src/app/(with-visible-layout)/kontakt/page.tsx
@@ -9,6 +9,7 @@ import lenkaCernouskovaUrl from '/public/images/team/lenka-cernouskova.jpg';
 import kristynaZizkovaUrl from '/public/images/team/kristyna-zizkova.jpg';
 import { imagePosition, UserHead } from './_components/UserHead';
 import { StaticImageData } from 'next/image';
+import { ContactForm } from './_components/ContactForm';
 
 export const metadata = {
   title: 'Kontakt',
@@ -59,24 +60,81 @@ const teamMembers: {
   },
 ];
 
+const locations = [
+  {
+    name: 'Prodejna v Hradci',
+    address: {
+      city: 'Hradec Kralové',
+      postCode: '500 03',
+      street: 'Tomkova 1230/4a',
+    },
+    map: {
+      location:
+        'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2553.3877819242625!2d15.8321447!3d50.2099736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470c2b2d912fc907%3A0xad7818771f15e6cc!2s4a%2C%20Tomkova%201230%2F4A%2C%20500%2003%20Hradec%20Kr%C3%A1lov%C3%A9!5e0!3m2!1sen!2scz!4v1691750049156!5m2!1sen!2scz',
+    },
+  },
+];
+
 const ContactPage: FC = () => {
   return (
-    <div className="px-4 my-20" id="nas-team">
-      <div className="mx-auto text-center">
-        <h2 className="text-5xl font-bold mt-10 mb-7">Náš tým</h2>
+    <>
+      <div className="container">
+        <div className="mx-auto">
+          <h2 className="text-5xl font-bold mt-10 mb-7">Kontakt</h2>
+        </div>
+        <div>
+          <ContactForm />
+        </div>
       </div>
-      <div className="flex flex-wrap items-center mx-auto justify-center gap-5 max-w-7xl">
-        {teamMembers.map((memberInfo) => (
-          <UserHead
-            key={memberInfo.name}
-            image={memberInfo.imageUrl}
-            name={memberInfo.name}
-            position={memberInfo.title}
-            imagePosition={memberInfo.imagePosition}
-          />
-        ))}
+
+      <div className="container" id="nas-team">
+        <div className="mx-auto">
+          <h2 className="text-5xl font-bold mt-10 mb-7">Náš tým</h2>
+        </div>
+        <div className="flex flex-wrap mx-auto gap-5">
+          {teamMembers.map((memberInfo) => (
+            <UserHead
+              key={memberInfo.name}
+              image={memberInfo.imageUrl}
+              name={memberInfo.name}
+              position={memberInfo.title}
+              imagePosition={memberInfo.imagePosition}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+
+      <div className="container">
+        <div className="mx-auto">
+          <h2 className="text-5xl font-bold mt-10 mb-7">Prodejny</h2>
+        </div>
+        <div className="grid grid-cols-4">
+          {locations.map((item) => (
+            <div className="p-2 bg-white rounded-lg shadow-lg">
+              <iframe
+                className="w-full top-0 left-0 rounded-lg shadow-white bg-white aspect-square"
+                scrolling="no"
+                frameBorder={0}
+                src={item.map.location}
+              ></iframe>
+
+              <div className="p-2">
+                <p className="text-deep-green-300 text-xl font-semibold mt-4">
+                  {item.name}
+                </p>
+
+                <address className="block mt-1 font-normal not-italic">
+                  {item.address.street} <br />
+                  {item.address.city}
+                  <br />
+                  {item.address.postCode}
+                </address>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
