010180888e531df4b3b6d79d0722f678bee7495f
Ondřej Langr <hi@ondrejlangr.cz>
2023-08-20 23:29:54 +0200

    Fix avatar by removing min(1) rule set by zodImage globally

--- packages/najit-najist-api/src/schemas/zodImage.ts
+++ packages/najit-najist-api/src/schemas/zodImage.ts
@@ -2,13 +2,10 @@ import { IMAGE_FILE_REGEX } from '@constants';
 import { isFileBase64 } from '@utils/isFileBase64';
 import { z } from 'zod';
 
-export const zodImage = z
-  .string()
-  .min(1)
-  .refine((input) => {
-    if (input.startsWith('data:')) {
-      return isFileBase64(input, IMAGE_FILE_REGEX);
-    }
+export const zodImage = z.string().refine((input) => {
+  if (input.startsWith('data:')) {
+    return isFileBase64(input, IMAGE_FILE_REGEX);
+  }
 
-    return true;
-  });
+  return true;
+});
