a18249be75f7aed275d920c3c84ef35f9578b5ba
Ondřej Langr <hi@ondrejlangr.cz>
2023-08-30 00:41:11 +0200

    Fix logger build

--- packages/najit-najist-api/src/logger.ts
+++ packages/najit-najist-api/src/logger.ts
@@ -1,8 +1,10 @@
 import pino from 'pino';
 
-const transport = pino.transport({
-  target: '@logtail/pino',
-  options: { sourceToken: process.env.NEXT_PUBLIC_LOGTAIL_TOKEN },
-});
+const transport = process.env.NEXT_PUBLIC_LOGTAIL_TOKEN
+  ? pino.transport({
+      target: '@logtail/pino',
+      options: { sourceToken: process.env.NEXT_PUBLIC_LOGTAIL_TOKEN },
+    })
+  : undefined;
 
 export const logger = pino(transport);
