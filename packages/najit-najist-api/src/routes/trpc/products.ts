7ca24d64eddd5c9244f61fd180bb3a4b83b408a3
Ondřej Langr <hi@ondrejlangr.cz>
2023-09-19 22:17:34 +0200

    Revalidate products on morph

--- packages/najit-najist-api/src/routes/trpc/products.ts
+++ packages/najit-najist-api/src/routes/trpc/products.ts
@@ -7,6 +7,7 @@ import {
 import { ProductService } from '@services/Product.service';
 import { t } from '@trpc';
 import { onlyAdminProcedure } from '@trpc-procedures/protectedProcedure';
+import { revalidatePath } from 'next/cache';
 import { z } from 'zod';
 
 const getRoutes = t.router({
@@ -22,10 +23,22 @@ export const productsRoutes = t.router({
   get: getRoutes,
   create: onlyAdminProcedure
     .input(createProductSchema)
-    .mutation(({ input, ctx }) =>
-      ProductService.create({ ...input, createdBy: ctx.sessionData.userId })
-    ),
+    .mutation(async ({ input, ctx }) => {
+      const result = await ProductService.create({
+        ...input,
+        createdBy: ctx.sessionData.userId,
+      });
+
+      revalidatePath(`/produkty`);
+      return result;
+    }),
   update: onlyAdminProcedure
     .input(z.object({ id: z.string(), payload: updateProductSchema }))
-    .mutation(({ input }) => ProductService.update(input.id, input.payload)),
+    .mutation(async ({ input }) => {
+      const result = await ProductService.update(input.id, input.payload);
+      revalidatePath(`/produkty/${result.slug}`);
+      revalidatePath(`/produkty`);
+
+      return result;
+    }),
 });
