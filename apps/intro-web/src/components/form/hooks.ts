eafcf638306671bbecf9da40861c936945d7c536
Ondřej Langr <hi@ondrejlangr.cz>
2023-09-20 16:15:58 +0200

    Remove intro-web as it is already deprecated

--- apps/intro-web/src/components/form/hooks.ts
+++
@@ -1,9 +0,0 @@
-import clsx from 'clsx';
-
-export const useFormClassNames = () => ({
-  label: clsx('block text-sm'),
-  input: clsx(
-    'bg-[#e1e7e3] text-lg focus:outline-none p-2 pl-4 focus:bg-white focus:ring-1 focus:ring-[#87a893] rounded-md w-full placeholder:text-[#bebebe] mt-1.5'
-  ),
-  error: clsx('text-red-500'),
-});
--- apps/intro-web/src/env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="@astrojs/image/client" />
-
-interface Window {
-  dataLayer: any[];
-  expandGtag(type: 'js', date: Date): void;
-  expandGtag(type: 'config', id: string): void;
-}
