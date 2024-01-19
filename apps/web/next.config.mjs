421ae086aef09eb48b4a2e38a46acfc1b6328feb
Ondřej Langr <hi@ondrejlangr.cz>
2024-01-19 22:07:53 +0100

    Fixes next config transpilePackages

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -7,10 +7,10 @@ const nextConfig = {
     serverActions: true,
   },
   transpilePackages: [
-    '@najitnajist/ui',
-    '@najitnajist/api',
-    '@najitnajist/pb',
-    '@najitnajist/email-templates',
+    '@najit-najist/ui',
+    '@najit-najist/api',
+    '@najit-najist/pb',
+    '@najit-najist/email-templates',
     '@najit-najist/tailwind-plugin',
   ],
   webpack(config, { isServer }) {
