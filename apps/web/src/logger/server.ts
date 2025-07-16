7bec47294fb83a8c7d2de7f5283dd7d524504de9
Ondřej Langr <hi@ondrejlangr.cz>
2025-07-16 09:45:08 +0200

    Updates logtail and logger implementation

--- apps/web/src/logger/server.ts
+++ apps/web/src/logger/server.ts
@@ -1,5 +1,33 @@
 import { Node as Logtail } from '@logtail/js';
+import type { StackContextHint } from '@logtail/types';
 
-export const logger = process.env.NEXT_PUBLIC_LOGTAIL_TOKEN
+const logtail = process.env.NEXT_PUBLIC_LOGTAIL_TOKEN
   ? new Logtail(process.env.NEXT_PUBLIC_LOGTAIL_TOKEN, {})
-  : ({} as InstanceType<typeof Logtail>);
+  : undefined;
+
+const stackContextHint: StackContextHint = {
+  fileName: 'node_modules/pino',
+  methodNames: [
+    'log',
+    'fatal',
+    'error',
+    'warn',
+    'info',
+    'debug',
+    'trace',
+    'silent',
+  ],
+  required: true,
+};
+
+export const logger = {
+  error: (message: string, meta?: Record<string, any>) => {
+    logtail?.log(message, 'error', meta, stackContextHint);
+  },
+  warn: (message: string, meta?: Record<string, any>) => {
+    logtail?.log(message, 'warn', meta, stackContextHint);
+  },
+  info: (message: string, meta?: Record<string, any>) => {
+    logtail?.log(message, 'info', meta, stackContextHint);
+  },
+};
